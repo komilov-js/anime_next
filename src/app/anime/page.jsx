859c92f819cb6ad27f2e3af17c9ee36750f2d3e6
fayzulloxo8 <solijonovfayzullox@gmail.com>
2025-11-01 16:42:56 +0500

    .

--- src/app/anime/page.jsx
+++ src/app/anime/page.jsx
@@ -172,7 +172,10 @@ const PageAnime = () => {
                 <div className="card-img">
                   <div className="card-text">
                     <div className="year"><p>{item.year || "2024"}</p></div>
-                    <div className="view-count"><p><svg data-v-7d80f126="" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="eye" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className="svg-inline--fa fa-eye fa-w-18"><path data-v-7d80f126="" fill="currentColor" d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z" className=""></path></svg> {item.views_count || 0}</p></div>
+                    <div className="view-count"><p><svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="white" className="bi bi-eye" viewBox="0 0 16 16">
+                      <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
+                      <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
+                    </svg>{item.views_count || 0}</p></div>
                   </div>
                   <img
                     src={
